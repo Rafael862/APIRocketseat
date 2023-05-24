6d525e6f3adf97a878b07800d0dd832c9cf1a5a4
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-05-23 21:25:16 -0300

    Finalizado as atividades do módulo 2

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors");
+
+const AppError = require("./utils/AppError");
 const express = require('express'); //aqui importa os módulos da pasta express em node_modules
 
 const routes = require("./routes");
@@ -6,6 +9,20 @@ const app = express(); //aqui faz a execução do express
 app.use(express.json()); //informa que o conteúdo vai ser no formato JSON
 
 app.use(routes);
+
+app.use((error, request, response, next) =>{
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+    console.error(error);
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    });
+});
 
 const PORT = 3333; //porta de acesso ao servidor
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); //faz a impressão dessa mensagem no console
