2c5ff2ebed8dc8022b8962fd8d8e56c793b8fa8f
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-05-29 20:53:17 -0300

    parei no erro

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,60 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
-class UsersController { 
-    create(request, response){
-        const { name, email, password} = request.body;
+const sqliteConnection = require("../database/sqlite");
 
-        if(!name){
-            throw new AppError("O nome é obrigatório");
+class UsersController { 
+    async create(request, response){
+        const { name, email, password} = request.body;
+        const database = await sqliteConnection();
+
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(checkUserExists){
+            throw new AppError("Este email já está em uso.");
         }
 
-        response.json({ name, email, password });
+        const hashedPassword = await hash(password, 8);
+
+        await database.run(
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+        [ name, email, hashedPassword ]
+        );
+
+        return response.status(201).json();
+       
+    }
+
+    async update(request, response){
+        const { name, email } = request.body;
+        const { id } = request.params;
+        console.log("teste", request) 
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if(!user){
+            throw new AppError("Usuário não encontrato");
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+            throw new AppError("Este email já está em uso.");
+        }
+        user.name = name;
+        user.email = email;
+
+        await database.run(`
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        updated_at = ?
+        WHERE id = ?`,
+        [user.name, user.email, new Date(), id]
+        );
+
+        return response.json();
     }
 
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,7 +5,8 @@ const UsersController = require("../controllers/UsersController");
 const usersRoutes = Router();
 
 
-const usersController = new UsersController();
+const usersController = new UsersController();  
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
-module.exports = usersRoutes;
+module.exports = usersRoutes; 
